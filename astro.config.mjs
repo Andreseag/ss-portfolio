752d3806c7038cf33eee558be376bc69cc783102
Edwin Castro <edwin.castro@agenciamatch.cl>
2026-01-28 08:53:36 -0500

    fix: fix error in username value :bug:

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://tu-usuario.github.io",
+  site: "https://andreseag.github.io",
   base: "/ss-portfolio",
   vite: {
     plugins: [tailwindcss()],
