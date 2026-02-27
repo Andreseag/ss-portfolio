0789b81213a5199ea694abe38d9fd23cd65032c4
Edwin Castro <edwin.castro@agenciamatch.cl>
2026-02-26 23:09:12 -0500

    feat: add news implementations :sparkles:

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://andreseag.github.io",
-  base: "/ss-portfolio",
+  base: "/",
   output: "static",
   vite: {
     plugins: [tailwindcss()],
